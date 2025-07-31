5c71fbe8dabaaa78867617372e7a5ba17d653f24
unknown <kauan.mazetorodrigues@gmail.com>
2025-07-30 22:11:21 -0300

    add file index.js

---
+++ index.js
@@ -0,0 +1,19 @@
+import express from 'express';
+
+const app = express();
+
+app.use(express.json());
+
+app.get('/usuarios', (req, res) => {
+    res.status(200).json({menssagem: 'Rota de GET- usuários'});
+});
+
+app.get('/usuarios/:id', (req, res) => {
+    const { id } = req.params;
+    res.status(200).json({menssagem: `Rota de GET- usuário ${id}`});
+});
+
+app.post('/usuarios/criar', (req, res) => {
+    const { nome, email } = req.body;
+    res.status(201).json({menssagem: `Usuário ${nome} criado com sucesso!`, email});
+});
